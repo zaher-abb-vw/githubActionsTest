21c9fd0cc2f620b3a15645e8aedb21a2173f3bf0
WP37IXX <zabboud@deloitte.de>
2024-03-26 11:31:50 +0100

    fix test python script 2

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import time
 print(time.time())
 print(time.localtime())
 print("test")
+
+# script.py
+
+import requests
+
+# Example: Make a GET request to a URL
+response = requests.get('https://api.github.com')
+print(response.json())
